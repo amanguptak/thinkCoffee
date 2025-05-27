4f8398b43dd4adac73e0c1ad7360149e51c670ff
Aman Gupta <eagleax7@gmail.com>
2025-05-27 15:49:33 +0530

    homescreen data setup added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useState} from 'react';
+import {Text, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useMyStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+
+const getCategoriesFromData = (data: any) => {
+  if (!data.length) return;
+
+  const categoryCount = data.reduce(
+    (final: Record<string, number>, curr: any) => {
+      final[curr.name] === undefined
+        ? (final[curr.name] = 1)
+        : (final[curr.name] += 1);
+
+      return final;
+    },
+    {},
+  );
+
+  const categories = Object.keys(categoryCount);
+  categories.unshift('All');
+  return categories;
+};
+
+const getCoffeeList = (category: string, data: any) => {
+  if (category == 'All') {
+    return data;
+  } else {
+    let coffeeList = data.filter((item: any) => item.name == category);
+    return coffeeList;
+  }
+};
 
 const HomeScreen = () => {
+  const coffeeList = useMyStore((state: any) => state.CoffeeList);
+  const beanList = useMyStore((state: any) => state.BeanList);
+  console.log(coffeeList, 'and', beanList);
+
+  const [categories, setCategories] = useState<T['']>(
+    getCategoriesFromData(coffeeList),
+  );
+  const [searchText, setSearchText] = useState(undefined);
+  const [categoryIndex, setCategoryIndex] = useState({
+    index: 0,
+    category: categories[0],
+  });
+
+  const [sortedCoffe, setSortedCoffee] = useState(
+    getCoffeeList(categoryIndex.category, coffeeList),
+  );
   return (
     <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
       <Text>Welcome to Home Screen</Text>
@@ -15,6 +61,5 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,12 @@
 import { create } from "zustand";
-
 import { produce } from "immer";
 import {persist,createJSONStorage} from 'zustand/middleware'
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import CoffeeData from "../data/CoffeeData";
 import BeansData from "../data/BeansData";
 
 
-export const useStore = create(
+export const useMyStore = create(
     persist(
         (set,get)=>({
             CoffeeList:CoffeeData,
